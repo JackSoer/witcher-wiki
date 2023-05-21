76cf9e05b21c91aa36c3229682e501ab5a307cff
JackSoer <kirillrotko1@gmail.com>
2023-05-21 13:37:34 +0300

    Added environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD6enI6wg6QUj7gVY5T-v6AVuAGOrjaYp4',
-  authDomain: 'witcher-wiki-f27b2.firebaseapp.com',
-  projectId: 'witcher-wiki-f27b2',
-  storageBucket: 'witcher-wiki-f27b2.appspot.com',
-  messagingSenderId: '753526703697',
-  appId: '1:753526703697:web:8661a277ee0a380809da70',
-  measurementId: 'G-C07QJXLEB5',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
